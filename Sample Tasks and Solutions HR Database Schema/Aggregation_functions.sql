33ead8052de5cec8460796cb956029d7d41b60f7
sylwiagoj <sylwia.k.goj@gmail.com>
2024-07-26 21:31:23 +0200

    A subquery - Aggregation_functions

---
+++ Sample Tasks and Solutions HR Database Schema/Aggregation_functions.sql
@@ -0,0 +1,57 @@
+--Aggregation functions--
+
+--1. Provide the highest and lowest salary among all employees. / Podaj najwy¿sz¹ i najni¿sz¹ pensjê wœród wszystkich pracowników.
+
+	SELECT 
+	MAX(salary) AS highest_salary, 
+	MIN(salary) AS lowest_salary
+	FROM employees;
+
+
+--2. Calculate the total salary paid to employees in each department. / Oblicz ³¹czn¹ pensjê wyp³acon¹ pracownikom w ka¿dym dziale.
+
+	SELECT department_id, SUM(salary) AS total_salary
+	FROM employees
+	GROUP BY department_id;
+	
+--3. Show the number of employees for each job position (job_id). / Przedstaw liczbê pracowników na ka¿dym stanowisku (job_id).
+
+	SELECT job_id, COUNT(*) AS number_of_employees
+	FROM employees
+	GROUP BY job_id;
+	
+--4. Calculate the number of employees in each department with a salary exceeding 5000. / Oblicz liczbê pracowników w ka¿dym dziale, których pensja przekracza 5000.
+
+	SELECT department_id, COUNT(*) AS number_of_high_salary_employees
+	FROM employees
+	WHERE salary > 5000
+	GROUP BY department_id;
+	
+--5. Calculate the total salary in departments where the average salary exceeds 6000. / Oblicz sumê pensji w dzia³ach, gdzie œrednia pensja przekracza 6000.
+
+	SELECT department_id, 
+       SUM(salary) AS total_salary
+	FROM employees
+	GROUP BY department_id
+	HAVING AVG(salary) > 6000;
+
+
+--6. Provide the earliest and latest hire date of employees in each department. Sort the results by the earliest hire date from shortest to longest duration. /
+-- Podaj najwczeœniejsz¹ i najpóŸniejsz¹ datê zatrudnienia pracowników w ka¿dym dziale. 
+-- Posegreguj wyniki wed³ug najwczeœniejszej daty zatrudnienia od najkrótszego do najd³u¿szego czasu.
+
+	SELECT department_id, 
+		MIN(hire_date) AS earliest_hire_date, 
+		MAX(hire_date) AS latest_hire_date
+	FROM employees
+	GROUP BY department_id
+	ORDER BY MIN(hire_date) ASC;	
+
+
+--7. Calculate the average length of employment (in days) for employees in each department and round the results to the nearest whole number. / 
+-- Oblicz œredni¹ d³ugoœæ zatrudnienia pracowników (w dniach) w ka¿dym dziale i zaokr¹glij wyniki do najbli¿szej liczby ca³kowitej.
+
+	SELECT department_id, 
+		ROUND(AVG(SYSDATE - hire_date)) AS average_tenure
+	FROM employees
+	GROUP BY department_id;
